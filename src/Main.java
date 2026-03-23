1a42ed6e8c409dd3272e5296afa61f921d416584
normatime <normanomoretime@gmail.com>
2026-03-23 18:03:27 +0300

    homework5 test

---
+++ src/Main.java
@@ -0,0 +1,79 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // task 1
+
+        int age = 18;
+
+        if (age >= 18) {
+
+            System.out.println("Если возраст человека равен " + age + " то он совершеннолетний.");
+
+
+        } else {
+            System.out.println("Если возраст человека равен " + age + " то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+
+// task 2
+
+        int temperature = 11;
+        if (temperature < 5) {
+
+            System.out.println("На улице холодно, нужно надеть шапку.");
+        }
+if (temperature > 5) {
+
+    System.out.println("Сегодня тепло, можно идти без шапки.");
+}
+
+        // task 3
+
+        int speed = 79;
+if (speed > 60) {
+
+    System.out.println("Если скорость " + speed + " то придётся заплатить штраф.");
+}
+    if (speed <60) {
+
+        System.out.println("Если скорость " + speed + " то можно ездить спокойно.");
+    }
+
+        //task 4
+
+        int agee = 4;
+    if (agee >2 && agee <6 ) {
+        System.out.println("Если возраст человека равен " + agee + " то ему нужно ходить в сад.");
+
+    }
+agee = 10;
+    if (agee > 7 && agee <17) {
+
+        System.out.println("Если возраст человека равен " + agee + " то ему нужно ходить в школу. ");
+    }
+
+    agee = 21;
+    if (agee >18 && agee <24) {
+
+        System.out.println("Если возраст человека равен " + agee + " то ему нужно ходить в университет.");
+    }
+
+    agee = 25;
+    if (agee >25) {
+
+        System.out.println("Если возраст человека больше  " + agee + " то ему нужно ходить на работу.");
+    }
+
+
+        // task 5
+
+
+
+
+    }
+
+
+}
+
+
